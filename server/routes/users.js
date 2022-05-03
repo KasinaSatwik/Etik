c137292cde35415d9a4b17347098e1d6ae515a83
KasinaSatwik <satwik.kasina@gmail.com>
2022-05-03 21:32:14 +0530

    resolved security vulnerabilities

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,7 +8,7 @@ router.post("/", async (req, res) => {
 		if (error)
 			return res.status(400).send({ message: error.details[0].message });
 
-		const user = await User.findOne({ email: req.body.email });
+		const user = await User.findOne({ email: req.body.email.toString() });
 		if (user)
 			return res
 				.status(409)
