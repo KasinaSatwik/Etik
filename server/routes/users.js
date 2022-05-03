9fb95962491943df10483c9d50451342ae631eb6
KasinaSatwik <satwik.kasina@gmail.com>
2022-05-03 21:31:03 +0530

    check

--- server/routes/users.js
+++ server/routes/users.js
@@ -8,7 +8,7 @@ router.post("/", async (req, res) => {
 		if (error)
 			return res.status(400).send({ message: error.details[0].message });
 
-		const user = await User.findOne({ email: req.body.email.toString() });
+		const user = await User.findOne({ email: req.body.email });
 		if (user)
 			return res
 				.status(409)
